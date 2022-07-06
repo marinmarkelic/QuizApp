54332cb5060a280731a4bce70fde88c015a027ae
marinmarkelic <marin.markelic.ets@gmail.com>
2022-07-06 14:41:30 +0200

    Removed comments and fixed code style issues

--- QuizApp/QuizApp/AppDelegate.swift
+++ QuizApp/QuizApp/AppDelegate.swift
@@ -1,10 +1,3 @@
-//
-//  AppDelegate.swift
-//  QuizApp
-//
-//  Created by Marin on 06.07.2022..
-//
-
 import UIKit
 
 @main
@@ -14,10 +7,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         let window = UIWindow(frame: UIScreen.main.bounds)
-        window.rootViewController = ViewController() // Your initial view controller.
+        window.rootViewController = ViewController()
         window.makeKeyAndVisible()
+
         self.window = window
         return true
     }
-}
 
+}
