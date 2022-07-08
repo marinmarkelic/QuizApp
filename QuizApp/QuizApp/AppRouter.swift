48f8cb67a6c8879ad4354e6ea6a20b9708159466
marinmarkelic <marin.markelic.ets@gmail.com>
2022-07-08 16:42:15 +0200

    PR fix

--- QuizApp/QuizApp/AppRouter.swift
+++ QuizApp/QuizApp/AppRouter.swift
@@ -2,15 +2,15 @@ import UIKit
 
 class AppRouter: AppRouterProtocol {
 
-    private let navigationController: UINavigationController!
+    private let navigationController: UINavigationController
 
     init(navigationController: UINavigationController) {
         self.navigationController = navigationController
     }
 
     func showLogin() {
-        let loginVC = LoginViewController(viewModel: LoginViewModel())
-        navigationController.pushViewController(loginVC, animated: false)
+        let loginViewController = LoginViewController(viewModel: LoginViewModel())
+        navigationController.pushViewController(loginViewController, animated: true)
     }
 
 }
