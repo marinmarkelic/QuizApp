a54ee48b9f405167b8b1d60a5b3fc9d76c4b5dff
marinmarkelic <marin.markelic.ets@gmail.com>
2022-07-08 10:53:34 +0200

    Created LoginViewModel

---
+++ QuizApp/QuizApp/Login/LoginViewModel.swift
@@ -0,0 +1,27 @@
+import Combine
+
+class LoginViewModel {
+
+    @Published var isLoginButtonEnabled = false
+
+    private var isLoginEmailValid = false
+    private var isLoginPasswordValid = false
+
+    private var email = ""
+    private var password = ""
+
+    func updatedEmail(withText text: String){
+        email = text
+        checkInputValidity()
+    }
+
+    func updatedPassword(withText text: String){
+        password = text
+        checkInputValidity()
+    }
+
+    private func checkInputValidity() {
+        isLoginButtonEnabled = !password.isEmpty && !email.isEmpty
+    }
+
+}
