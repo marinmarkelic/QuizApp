607a20fcfe4d7234edad0290b648811bcf08b71b
marinmarkelic <marin.markelic.ets@gmail.com>
2022-07-11 16:33:10 +0200

    Added UserNetworkDataSource

---
+++ QuizApp/QuizApp/UserNetworkDataSource.swift
@@ -0,0 +1,24 @@
+class UserNetworkDataSource {
+
+    private var loginClient: LoginClientProtocol
+
+    private var loginResponseData: LoginResponseData!
+
+    init(loginClient: LoginClientProtocol) {
+        self.loginClient = loginClient
+    }
+
+    func logIn(username: String, password: String) async throws -> LoginResponseData {
+        return LoginResponseData(try await loginClient.logIn(username: username, password: password))
+    }
+}
+
+struct LoginResponseData {
+
+    let accessToken: String
+
+    init(_ loginResponse: LoginResponse) {
+        accessToken = loginResponse.accessToken
+    }
+
+}
