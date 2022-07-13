adccf5a000fcdcd11166531c0ba4dc2a1db7c797
marinmarkelic <marin.markelic.ets@gmail.com>
2022-07-13 15:59:57 +0200

    Added GradientView to QuizVC and SearchVC

--- QuizApp/QuizApp/Quiz/QuizViewController.swift
+++ QuizApp/QuizApp/Quiz/QuizViewController.swift
@@ -1,5 +1,38 @@
 import UIKit
+import SnapKit
 
-class QuizViewController: UIViewController {
+class QuizViewController: UIViewController, ConstructViewsProtocol {
+
+    private var gradientView: GradientView!
+
+    private var mainView: UIView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        createViews()
+        styleViews()
+        defineLayoutForViews()
+    }
+
+    func createViews() {
+        gradientView = GradientView()
+        view.addSubview(gradientView)
+
+        mainView = UIView()
+        gradientView.addSubview(mainView)
+    }
+
+    func styleViews() { }
+
+    func defineLayoutForViews() {
+        gradientView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+
+        mainView.snp.makeConstraints {
+            $0.edges.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
 
 }
