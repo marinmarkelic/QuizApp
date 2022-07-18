6a6c332db4673256fc45d20af97f3e17a3c1edf0
marinmarkelic <marin.markelic.ets@gmail.com>
2022-07-18 16:19:10 +0200

    Moved colors

--- QuizApp/QuizApp/User defined types/Quiz.swift
+++ QuizApp/QuizApp/User defined types/Quiz.swift
@@ -1,5 +1,10 @@
 import UIKit
 
+public let sportColor = UIColor(red: 242 / 255, green: 201 / 255, blue: 76 / 255, alpha: 1)
+public let moviesColor = UIColor(red: 86 / 255, green: 204 / 255, blue: 242 / 255, alpha: 1)
+public let musicColor = UIColor(red: 242 / 255, green: 24 / 255, blue: 24 / 255, alpha: 1)
+public let geographyColor = UIColor(red: 117 / 255, green: 74 / 255, blue: 21 / 255, alpha: 1)
+
 enum Difficulty: String {
 
     case easy = "EASY"
