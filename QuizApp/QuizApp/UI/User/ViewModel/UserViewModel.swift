4fa267150d92b0b9c04e8269eb631199a63f9f42
marinmarkelic <marin.markelic.ets@gmail.com>
2022-07-20 16:16:16 +0200

    Same

--- QuizApp/QuizApp/UI/User/ViewModel/UserViewModel.swift
+++ QuizApp/QuizApp/UI/User/ViewModel/UserViewModel.swift
@@ -43,33 +43,3 @@ class UserViewModel {
     }
 
 }
-
-struct UserInfo {
-
-    let username: String
-    let name: String
-
-}
-
-extension UserInfo {
-
-    init() {
-        username = ""
-        name = ""
-    }
-
-    init(_ userInfo: UserInfoModel) {
-        username = userInfo.username
-        name = userInfo.name
-    }
-
-}
-
-extension UserInfoModel {
-
-    init(_ userInfo: UserInfo) {
-        username = userInfo.username
-        name = userInfo.name
-    }
-
-}
