cdab914346b3bcd5ee2d969a05040f88c0968eab
marinmarkelic <marin.markelic.ets@gmail.com>
2022-07-21 17:12:41 +0200

    Fixed QuizNC and moved QuizNetwork models

--- QuizApp/QuizApp/Network/Clients/NetworkModels.swift
+++ QuizApp/QuizApp/Network/Clients/NetworkModels.swift
@@ -11,3 +11,32 @@ struct UserInfoNetworkRequestModel: Encodable {
     let name: String
 
 }
+
+struct QuizNetworkDataModel: Decodable {
+
+    let id: Int
+    let name: String
+    let description: String
+    let category: CategoryNetworkDataModel
+    let difficulty: DifficultyNetworkDataModel
+    let imageUrl: String
+    let numberOfQuestions: Int
+
+}
+
+enum CategoryNetworkDataModel: String, Decodable {
+
+    case sport = "SPORT"
+    case movies = "MOVIES"
+    case music = "MUSIC"
+    case geography = "GEOGRAPHY"
+
+}
+
+enum DifficultyNetworkDataModel: String, Decodable {
+
+    case easy = "EASY"
+    case normal = "NORMAL"
+    case hard = "HARD"
+
+}
--- QuizApp/QuizApp/Network/Clients/QuizNetworkClient.swift
+++ QuizApp/QuizApp/Network/Clients/QuizNetworkClient.swift
@@ -10,14 +10,14 @@ class QuizNetworkClient: QuizNetworkClientProtocol {
 
     private let networkClient: NetworkClient
 
-    private let quizPath = "/v1/quiz/list?category="
+    private let quizPath = "/v1/quiz/list"
 
     init(networkClient: NetworkClient) {
         self.networkClient = networkClient
     }
 
     func fetchQuizzes(for category: CategoryNetworkDataModel) async throws -> [QuizNetworkDataModel] {
-        try await networkClient.get(path: "\(quizPath)\(category.rawValue)")
+        try await networkClient.get(path: "\(quizPath)?category=\(category.rawValue)")
     }
 
     func fetchAllQuizzes() async throws -> [QuizNetworkDataModel] {
