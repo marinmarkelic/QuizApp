cec7fb453767f6a4ebb3ee9f96cd7ab0e61ed2ef
marinmarkelic <marin.markelic.ets@gmail.com>
2022-07-22 11:15:53 +0200

    Color

--- QuizApp/QuizApp/UI/Common/Colors.swift
+++ QuizApp/QuizApp/UI/Common/Colors.swift
@@ -5,7 +5,7 @@ extension UIColor {
     static let sportColor = UIColor(red: 242 / 255, green: 201 / 255, blue: 76 / 255, alpha: 1)
     static let moviesColor = UIColor(red: 86 / 255, green: 204 / 255, blue: 242 / 255, alpha: 1)
     static let musicColor = UIColor(red: 242 / 255, green: 24 / 255, blue: 24 / 255, alpha: 1)
-    static let geographyColor = UIColor(red: 171 / 255, green: 130 / 255, blue: 97 / 255, alpha: 1)
+    static let geographyColor = UIColor(red: 165 / 255, green: 212 / 255, blue: 121 / 255, alpha: 1)
     static let allColor = UIColor(red: 245 / 255, green: 158 / 255, blue: 66 / 255, alpha: 1)
 
 }
