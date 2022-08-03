3bcb70cef2e1e17c76016cdea321c2638ae84808
marinmarkelic <marin.markelic.ets@gmail.com>
2022-08-03 14:46:14 +0200

    Created colors in assets and changed static lets in UIColor extension

--- QuizApp/QuizApp/UI/Common/Colors.swift
+++ QuizApp/QuizApp/UI/Common/Colors.swift
@@ -2,13 +2,13 @@ import UIKit
 
 extension UIColor {
 
-    static let correctAnswerColor = UIColor(red: 111 / 255, green: 207 / 255, blue: 151 / 255, alpha: 1)
-    static let incorrectAnswerColor = UIColor(red: 252 / 255, green: 101 / 255, blue: 101 / 255, alpha: 1)
+    static let correctAnswerColor = UIColor(named: "CorrectAnswerColor") ?? .white
+    static let incorrectAnswerColor = UIColor(named: "IncorrectAnswerColor") ?? .white
 
-    static let sportColor = UIColor(red: 242 / 255, green: 201 / 255, blue: 76 / 255, alpha: 1)
-    static let moviesColor = UIColor(red: 86 / 255, green: 204 / 255, blue: 242 / 255, alpha: 1)
-    static let musicColor = UIColor(red: 242 / 255, green: 24 / 255, blue: 24 / 255, alpha: 1)
-    static let geographyColor = UIColor(red: 165 / 255, green: 212 / 255, blue: 121 / 255, alpha: 1)
-    static let allColor = UIColor(red: 245 / 255, green: 158 / 255, blue: 66 / 255, alpha: 1)
+    static let sportColor = UIColor(named: "SportColor") ?? .white
+    static let moviesColor = UIColor(named: "MoviesColor") ?? .white
+    static let musicColor = UIColor(named: "MusicColor") ?? .white
+    static let geographyColor = UIColor(named: "GeographyColor") ?? .white
+    static let allColor = UIColor(named: "AllColor") ?? .white
 
 }
