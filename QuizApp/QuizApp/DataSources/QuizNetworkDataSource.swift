9407b00ee0704f3bdb3168648fcb3d42c0c9107b
marinmarkelic <marin.markelic.ets@gmail.com>
2022-08-16 10:11:39 +0200

    Renamed quizNetworkClient to networkClient

--- QuizApp/QuizApp/DataSources/QuizNetworkDataSource.swift
+++ QuizApp/QuizApp/DataSources/QuizNetworkDataSource.swift
@@ -14,37 +14,37 @@ protocol QuizNetworkDataSourceProtocol {
 
 class QuizNetworkDataSource: QuizNetworkDataSourceProtocol {
 
-    private let quizNetworkClient: QuizNetworkClientProtocol
+    private let networkClient: QuizNetworkClientProtocol
 
-    init(quizNetworkClient: QuizNetworkClientProtocol) {
-        self.quizNetworkClient = quizNetworkClient
+    init(networkClient: QuizNetworkClientProtocol) {
+        self.networkClient = networkClient
     }
 
     func fetchQuizzes(for category: CategoryDataModel) async throws -> [QuizResponseDataModel] {
-        let quizzes = try await quizNetworkClient.fetchQuizzes(
+        let quizzes = try await networkClient.fetchQuizzes(
             for: CategoryNetworkDataModel(rawValue: category.rawValue)!)
         return quizzes
             .map { QuizResponseDataModel($0) }
     }
 
     func fetchAllQuizzes() async throws -> [QuizResponseDataModel] {
-        let quizzes = try await quizNetworkClient.fetchAllQuizzes()
+        let quizzes = try await networkClient.fetchAllQuizzes()
         return quizzes
             .map { QuizResponseDataModel($0) }
     }
 
     func fetchLeaderboard(for id: Int) async throws -> LeaderboardDataModel {
-        let response = try await quizNetworkClient.fetchLeaderboard(for: id)
+        let response = try await networkClient.fetchLeaderboard(for: id)
         return LeaderboardDataModel(response)
     }
 
     func startQuiz(with request: QuizStartRequestDataModel) async throws -> QuizStartResponseDataModel {
-        let response = try await quizNetworkClient.startQuiz(with: QuizStartRequestNetworkDataModel(request))
+        let response = try await networkClient.startQuiz(with: QuizStartRequestNetworkDataModel(request))
         return QuizStartResponseDataModel(response)
     }
 
     func endQuiz(with request: QuizEndRequestDataModel) async throws -> QuizEndResponseDataModel {
-        let response = try await quizNetworkClient.endQuiz(
+        let response = try await networkClient.endQuiz(
             with: QuizEndRequestNetworkDataModel(request),
             id: request.id)
         return QuizEndResponseDataModel(response)
