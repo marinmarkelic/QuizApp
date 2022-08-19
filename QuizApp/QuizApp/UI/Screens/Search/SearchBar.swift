f1abf42c5db345c3b7c4d9a99d6fbc055bd6a11f
marinmarkelic <marin.markelic.ets@gmail.com>
2022-08-19 16:10:47 +0200

    Added textfield unfocussing

--- QuizApp/QuizApp/UI/Screens/Search/SearchBar.swift
+++ QuizApp/QuizApp/UI/Screens/Search/SearchBar.swift
@@ -4,24 +4,24 @@ struct SearchBar: View {
 
     @Binding var text: String
 
+    @FocusState var isTextFieldFocused: Bool
+
     var onSearchTap: () -> Void = {}
 
     var body: some View {
         HStack {
             RoundedTextField(placeholder: "Type here", isSecure: false, text: $text)
+                .focused($isTextFieldFocused)
 
             Text("Search")
                 .font(.heading6)
                 .foregroundColor(.white)
                 .onTapGesture {
                     onSearchTap()
+                    isTextFieldFocused = false
                 }
         }
     }
-
-}
-
-extension SearchBar {
 
     func onSearchTap(_ onSearchTap: @escaping () -> Void) -> SearchBar {
         SearchBar(text: $text, onSearchTap: onSearchTap)
