41f8d81a736ee55c7b26ab3395a00618c6c6b239
marinmarkelic <marin.markelic.ets@gmail.com>
2022-08-22 15:47:20 +0200

    ProgressView and UIKit+extensions

---
+++ QuizApp/QuizApp/UI/Screens/SolvingQuiz/ProgressView.swift
@@ -0,0 +1,25 @@
+import SwiftUI
+
+struct ProgressView: View {
+
+    let progressText: String
+    let progressColors: [UIColor]
+
+    var body: some View {
+        VStack {
+            Text(progressText)
+                .font(.heading5)
+                .foregroundColor(.white)
+                .maxWidth(alignment: .leading)
+
+            HStack(spacing: 5) {
+                ForEach(progressColors) {color in
+                    Rectangle()
+                        .foregroundColor(Color(color))
+                        .frame(height: 2)
+                        .cornerRadius(2)
+                }
+            }
+        }
+    }
+}
--- QuizApp/QuizApp/UI/Screens/SolvingQuiz/SolvingQuizView.swift
+++ QuizApp/QuizApp/UI/Screens/SolvingQuiz/SolvingQuizView.swift
@@ -25,29 +25,3 @@ struct SolvingQuizPreview: PreviewProvider {
     }
 
 }
-
-struct ProgressView: View {
-
-    let progressText: String
-    let progressColors: [UIColor]
-
-    var body: some View {
-        VStack {
-            Text(progressText)
-                .font(.heading5)
-                .foregroundColor(.white)
-                .maxWidth(alignment: .leading)
-
-            HStack(spacing: 5) {
-                ForEach(progressColors) {color in
-                    Rectangle()
-                        .foregroundColor(Color(color))
-                        .frame(height: 2)
-                        .cornerRadius(2)
-                }
-            }
-        }
-    }
-}
-
-extension UIColor: Identifiable {}
