d714cad10b81961c208ec906ffd6c30a94b76ca3
marinmarkelic <marin.markelic.ets@gmail.com>
2022-08-22 15:25:21 +0200

    Created SolvingQuizView

---
+++ QuizApp/QuizApp/UI/Screens/SolvingQuiz/SolvingQuizView.swift
@@ -0,0 +1,19 @@
+import SwiftUI
+
+struct SolvingQuizView: View {
+
+    var body: some View {
+        Text("a")
+    }
+
+}
+
+struct SolvingQuizPreview: PreviewProvider {
+
+    static var previews: some View {
+        SolvingQuizView()
+    }
+
+}
+
+
