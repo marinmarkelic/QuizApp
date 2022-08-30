1613b008543a180d4c4a3ea2317321154394b506
marinmarkelic <marin.markelic.ets@gmail.com>
2022-08-30 13:36:22 +0200

    Update SearchSelectionView.swift

--- QuizApp/QuizApp/UI/Screens/Home/SearchSelectionView.swift
+++ QuizApp/QuizApp/UI/Screens/Home/SearchSelectionView.swift
@@ -11,13 +11,22 @@ struct SearchSelectionView: View {
     var body: some View {
         VStack {
             UIPilotHost(quizzesPilot) { route in
-                switch route {
-                case .search: return AnyView(SearchView(viewModel: container.resolve()))
-                case .details(let quiz): return AnyView(QuizDetailsView(viewModel: container.resolve(args: quiz)))
-                case .solving(let quizId): return AnyView(SolvingQuizView(viewModel: container.resolve(args: quizId)))
-                case .finished(let results): return AnyView(QuizResultView(viewModel: container.resolve(args: results)))
-                }
+                AnyView(makeScreen(route))
             }
+        }
+    }
+
+    @ViewBuilder
+    private func makeScreen(_ route: SearchAppRoute) -> some View {
+        switch route {
+        case .search:
+            SearchView(viewModel: container.resolve())
+        case .details(let quiz):
+            QuizDetailsView(viewModel: container.resolve(args: quiz))
+        case .solving(let quizId):
+            SolvingQuizView(viewModel: container.resolve(args: quizId))
+        case .finished(let results):
+            QuizResultView(viewModel: container.resolve(args: results))
         }
     }
 
