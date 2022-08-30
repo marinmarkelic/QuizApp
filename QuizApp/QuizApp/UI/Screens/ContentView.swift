2f37c5072941074d617b12020b9bbbef1bb81e12
marinmarkelic <marin.markelic.ets@gmail.com>
2022-08-30 13:34:31 +0200

    AppData to separate folderr

--- QuizApp/QuizApp/UI/Screens/ContentView.swift
+++ QuizApp/QuizApp/UI/Screens/ContentView.swift
@@ -40,18 +40,3 @@ struct ContentView: View {
     }
 
 }
-
-class AppData: ObservableObject {
-
-    @Published var loginStatus: LoginStatus = .unknown
-    @Published var selectedTab: AppRoute = .quizzes
-
-}
-
-enum LoginStatus {
-
-    case unknown
-    case notLoggedIn
-    case loggedIn
-
-}
